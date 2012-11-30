c990e0cab0b467d798a3ecb9189ad5ce86c07354
AntGarSil <agdosil@gmail.com>
2012-11-30 03:21:51 +0100

    investigation on Stormers theorem

---
+++ problem66.rb
@@ -0,0 +1,12 @@
+#problem 66 from projecteuler.net
+
+#Consider the Diophantine equations of the form:
+										# x**2 - Dy**2 = 1
+# Find the value of D <= 1000 in minimal solutions of x for which the largest value of x is obtained
+
+#I must resolve this with Stormers Theorem
+
+def solveDiophantine(D, limit)
+
+			
+end
